9e1719f46f65ab4922174822e59fa6949bfd8988
MironovVS <MAdd2062@gmail.com>
2015-12-09 13:58:20 +0400

    перенёс в класс MSQL

--- model/startup.php
+++ model/startup.php
@@ -6,16 +6,18 @@ function startup()
 	$hostname = 'localhost'; 
 	$username = 'root'; 
 	$password = '';
-	$dbName = 'lesson';
+	$dbName = 'PHP2-7';
 	
 	// Языковая настройка.
 	setlocale(LC_ALL, 'ru_RU.utf8');	
 	
 	// Подключение к БД.
-	mysql_connect($hostname, $username, $password) or die('No connect with data base'); 
-	mysql_query('SET NAMES utf8');
-	mysql_select_db($dbName) or die('No data base');
+	$link=mysqli_connect($hostname, $username, $password) or die('No connect with data base');
+	mysqli_query($link,'SET NAMES utf8');
+	mysqli_select_db($link, $dbName) or die('No data base');
 
 	// Открытие сессии.
-	session_start();		
+	session_start();
+
+	return $link;
 }
