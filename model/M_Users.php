68b95a29e61158063e2442b19bf7b5ec1785894a
MironovVS <MAdd2062@gmail.com>
2015-12-10 20:51:36 +0400

    добавил метод регистрации пользователей

--- model/M_Users.php
+++ model/M_Users.php
@@ -5,40 +5,40 @@ include_once('model/MSQL.php');
 // Менеджер пользователей
 //
 class M_Users
-{	
-	private static $instance;	// экземпляр класса
-	private $msql;				// драйвер БД
-	private $sid;				// идентификатор текущей сессии
-	private $uid;				// идентификатор текущего пользователя
-	
+{
+	private static $instance;  // экземпляр класса
+	private $msql;        // драйвер БД
+	private $sid;        // идентификатор текущей сессии
+	private $uid;        // идентификатор текущего пользователя
+
 	//
 	// Получение экземпляра класса
 	// результат	- экземпляр класса MSQL
 	//
-	public static function Instance()
+	public static function Instance ()
 	{
 		if (self::$instance == null)
 			self::$instance = new M_Users();
-			
+
 		return self::$instance;
 	}
 
 	//
 	// Конструктор
 	//
-	public function __construct()
+	public function __construct ()
 	{
 		$this->msql = MSQL::Instance();
 		$this->sid = null;
 		$this->uid = null;
 	}
-	
+
 	//
 	// Очистка неиспользуемых сессий
 	// 
-	public function ClearSessions()
+	public function ClearSessions ()
 	{
-		$min = date('Y-m-d H:i:s', time() - 60 * 20); 			
+		$min = date('Y-m-d H:i:s', time() - 60 * 20);
 		$t = "time_last < '%s'";
 		$where = sprintf($t, $min);
 		$this->msql->Delete('sessions', $where);
@@ -51,54 +51,53 @@ class M_Users
 	// $remember 	- нужно ли запомнить в куках
 	// результат	- true или false
 	//
-	public function Login($login, $password, $remember = true)
+	public function Login ($login, $password, $remember = true)
 	{
 		// вытаскиваем пользователя из БД 
 		$user = $this->GetByLogin($login);
 
 		if ($user == null)
 			return false;
-		
+
 		$id_user = $user['id_user'];
-				
+
 		// проверяем пароль
 		if ($user['password'] != $password)
 			return false;
-				
+
 		// запоминаем имя и md5(пароль)
-		if ($remember)
-		{
+		if ($remember) {
 			$expire = time() + 3600 * 24 * 100;
 			setcookie('login', $login, $expire);
 			setcookie('password', $password, $expire);
-		}		
-				
+		}
+
 		// открываем сессию и запоминаем SID
 		$this->sid = $this->OpenSession($id_user);
-		
+
 		return true;
 	}
-	
+
 	//
 	// Выход
 	//
-	public function Logout()
+	public function Logout ()
 	{
 		setcookie('login', '', time() - 1);
 		setcookie('password', '', time() - 1);
 		unset($_COOKIE['login']);
 		unset($_COOKIE['password']);
-		unset($_SESSION['sid']);		
+		unset($_SESSION['sid']);
 		$this->sid = null;
 		$this->uid = null;
 	}
-						
+
 	//
 	// Получение пользователя
 	// $id_user		- если не указан, брать текущего
 	// результат	- объект пользователя
 	//
-	public function Get($id_user = null)
+	public function Get ($id_user = null)
 	{
 
 
@@ -107,73 +106,66 @@ class M_Users
 			$id_user = $this->GetUid();
 
 //		var_dump($id_user);
-			
+
 		if ($id_user == null)
 			return null;
-			
+
 		// А теперь просто возвращаем пользователя по id_user.
 		$t = "SELECT * FROM users WHERE id_user = '%d'";
 		$query = sprintf($t, $id_user);
 		$result = $this->msql->Select($query);
 
-			return $result[0];
+		return $result[0];
 
 
 	}
-	
+
 	//
 	// Получает пользователя по логину
 	//
-	public function GetByLogin($login)
-	{	
+	public function GetByLogin ($login)
+	{
 		$t = "SELECT * FROM users WHERE login = '%s'";
 		$query = sprintf($t, $login);
 		$result = $this->msql->Select($query);
 		return $result[0];
 	}
-			
+
 	//
 	// Проверка наличия привилегии
 	// $priv 		- имя привилегии
 	// $id_user		- если не указан, значит, для текущего
 	// результат	- true или false
 	//
-	public function Can($priv, $user = null)
+	public function Can ($priv, $user = null)
 	{
-		$user=$this->Get();
+		$user = $this->Get();
 //		var_dump($user['id_user']);
 
-				// СДЕЛАТЬ САМОСТОЯТЕЛЬНО
+		// СДЕЛАТЬ САМОСТОЯТЕЛЬНО
 		$t = "SELECT * FROM privs WHERE name = '%s'";
 		$query = sprintf($t, $priv);
 		$result_priv = $this->msql->Select($query); //Получаем id_priv из БД
-		$id_priv=$result_priv[0]['id_priv'];
-//		var_dump($id_priv); die();
+		$id_priv = $result_priv[0]['id_priv'];
+//		var_dump($id_priv);
 
 		$t_rol = "SELECT id_role FROM privs2roles WHERE id_priv = '%s'";
 		$query = sprintf($t_rol, $id_priv);
 		$result_rol = $this->msql->Select($query); //Получаем id_role из БД
 
-//		var_dump($result_rol[0]['id_role']); die();
+//		var_dump($result_rol); die();
 //		var_dump(count($result_rol)); die();
 
-		for ($i = 0 ; $i < count($result_rol); $i++) {
+		for ($i = 0; $i < count($result_rol); $i++) {
 			$t_user = "SELECT id_user FROM users WHERE id_role = '%s'";
 			$query = sprintf($t_user, $result_rol[$i]['id_role']);
 			$request = $this->msql->Select($query);
-//			var_dump($request[0]['id_user']);
+//			var_dump($request[0]['id_user']);die();
 //			var_dump($user['id_user']);
-			if ($request[0]['id_user'] == $user['id_user']) {
-					return true;
+			if ($request[$i]['id_user'] == $user['id_user']) {
+				return true;
 			}
 		}
-
-
-
-
-
-
-
 		return false;
 	}
 
@@ -182,30 +174,30 @@ class M_Users
 	// $id_user		- идентификатор
 	// результат	- true если online
 	//
-	public function IsOnline($id_user)
-	{		
+	public function IsOnline ($id_user)
+	{
 		// СДЕЛАТЬ САМОСТОЯТЕЛЬНО
 		return false;
 	}
-	
+
 	//
 	// Получение id текущего пользователя
 	// результат	- UID
 	//
-	public function GetUid()
-	{	
+	public function GetUid ()
+	{
 		// Проверка кеша.
 		if ($this->uid != null)
-			return $this->uid;	
+			return $this->uid;
 
 		// Берем по текущей сессии.
 		$sid = $this->GetSid();
 
 //		var_dump($sid);
-				
+
 		if ($sid == null)
 			return null;
-			
+
 		$t = "SELECT id_user FROM sessions WHERE sid = '%s'";
 		$query = sprintf($t, $sid);
 		$result = $this->msql->Select($query);
@@ -214,7 +206,7 @@ class M_Users
 		// Если сессию не нашли - значит пользователь не авторизован.
 		if (count($result) == 0)
 			return null;
-			
+
 		// Если нашли - запоминм ее.
 		$this->uid = $result[0]['id_user'];
 		return $this->uid;
@@ -224,80 +216,76 @@ class M_Users
 	// Функция возвращает идентификатор текущей сессии
 	// результат	- SID
 	//
-	private function GetSid()
+	private function GetSid ()
 	{
 		// Проверка кеша.
 		if ($this->sid != null)
 			return $this->sid;
-	
+
 		// Ищем SID в сессии.
 		$sid = $_SESSION['sid'];
 
 
-								
-		// Если нашли, попробуем обновить time_last в базе. 
+		// Если нашли, попробуем обновить time_last в базе.
 		// Заодно и проверим, есть ли сессия там.
-		if ($sid != null)
-		{
-			$session = array();
-			$session['time_last'] = date('Y-m-d H:i:s'); 			
+		if ($sid != null) {
+			$session = array ();
+			$session['time_last'] = date('Y-m-d H:i:s');
 			$t = "sid = '%s'";
 			$where = sprintf($t, $sid);
 			$affected_rows = $this->msql->Update('sessions', $session, $where);
 
-			if ($affected_rows == 0)
-			{
-				$t = "SELECT count(*) FROM sessions WHERE sid = '%s'";		
+			if ($affected_rows == 0) {
+				$t = "SELECT count(*) FROM sessions WHERE sid = '%s'";
 				$query = sprintf($t, $sid);
 				$result = $this->msql->Select($query);
-				
+
 				if ($result[0]['count(*)'] == 0)
-					$sid = null;			
-			}			
-		}		
-		
+					$sid = null;
+			}
+		}
+
 		// Нет сессии? Ищем логин и md5(пароль) в куках.
 		// Т.е. пробуем переподключиться.
-		if ($sid == null && isset($_COOKIE['login']))
-		{
+		if ($sid == null && isset($_COOKIE['login'])) {
 			$user = $this->GetByLogin($_COOKIE['login']);
 
 			if ($user != null && $user['password'] == $_COOKIE['password'])
 				$sid = $this->OpenSession($user['id_user']);
 
 		}
-		
+
 		// Запоминаем в кеш.
 		if ($sid != null)
 			$this->sid = $sid;
-		
+
 		// Возвращаем, наконец, SID.
-		return $sid;		
+		return $sid;
 	}
-	
+
 	//
 	// Открытие новой сессии
 	// результат	- SID
 	//
-	private function OpenSession($id_user)
+	private function OpenSession ($id_user)
 	{
 		// генерируем SID
 		$sid = $this->GenerateStr(10);
-				
+
 		// вставляем SID в БД
-		$now = date('Y-m-d H:i:s'); 
-		$session = array();
+		$now = date('Y-m-d H:i:s');
+		$session = array ();
 		$session['id_user'] = $id_user;
 		$session['sid'] = $sid;
 		$session['time_start'] = $now;
-		$session['time_last'] = $now;				
-		$this->msql->Insert('sessions', $session); 
-				
+		$session['time_last'] = $now;
+		$this->msql->Insert('sessions', $session);
+
 		// регистрируем сессию в PHP сессии
-		$_SESSION['sid'] = $sid;				
-				
+		$_SESSION['sid'] = $sid;
+
 		// возвращаем SID
-		return $sid;	
+		return $sid;
 	}
 
 	//
@@ -305,15 +293,42 @@ class M_Users
 	// $length 		- ее длина
 	// результат	- случайная строка
 	//
-	private function GenerateStr($length = 10) 
+	private function GenerateStr ($length = 10)
 	{
 		$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRQSTUVWXYZ0123456789";
 		$code = "";
-		$clen = strlen($chars) - 1;  
+		$clen = strlen($chars) - 1;
 
-		while (strlen($code) < $length) 
-            $code .= $chars[mt_rand(0, $clen)];  
+		while (strlen($code) < $length)
+			$code .= $chars[mt_rand(0, $clen)];
 
 		return $code;
+	}
+
+	public function register ($username, $pass, $id_role)
+	{
+			if (isset($_POST['username']) && $_POST['pass']) {
+			$username = $_POST['username'];
+			$pass = $_POST['pass'];
+			$id_role = $_POST['id_role'];
+
+			switch ($id_role) {
+				case "admin":
+					$id_role = "1";
+					break;
+				case "moder":
+					$id_role = "2";
+					break;
+				case "user":
+					$id_role = "3";
+					break;
+				default:
+					$id_role = "3";
+			}
+		}
+
+		$this->msql->Insert("users", array('login'=>"$username", 'password'=>"$pass", 'id_role'=>"$id_role", 'name'=>"$id_role"));
+
+
 	}
 }
